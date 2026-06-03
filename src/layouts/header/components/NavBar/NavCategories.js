086c7b92e32db03b5c5753de428cf5072bd1c1a3
vinaybadola <vinaybadola.cs18@gmail.com>
2026-06-04 00:32:32 +0530

    refactor: standardize URL paths for outstanding, billing, and receipt reports in nav categories

--- src/layouts/header/components/NavBar/NavCategories.js
+++ src/layouts/header/components/NavBar/NavCategories.js
@@ -8,9 +8,9 @@ export const navCategories = [
       { name: "Pcd Closure", path: "/billing/account/pcd-closure", moduleName: "PCD Ready order module", url: "/billing/sale/ready-order/all", action: ["READ"],  },  
       { name: "Terminate Orders", path: "/billing/account/terminate-orders", moduleName: "Terminate Orders ", url: "/billing/sale/ready-order/all", action: ["READ"],  }, 
       { name: "Order Links", path: "/billing/account/order-links", moduleName: "Order Links ", url: "/billing/sale/ready-order/all", action: ["READ"],  },   
-      { name: "Outstanding Report", path: "/billing/account/outstanding-report", moduleName: "Sale outstanding", url: "/billing/sale/monthly/orders/outstanding", action: ["READ"],  },
-      { name: "Billing sell Report", path: "/billing/account/billing-report", moduleName: "Billing Sale Access", url: "/billing/sale", action: ["READ"],  },
-      { name: "Receipt Report", path: "/billing/account/receipt-report", moduleName: "Receipt report", url: "/billing/sale/monthly/orders/receipt", action: ["READ"],  },
+      { name: "Outstanding Report", path: "/billing/account/outstanding-report", moduleName: "Sale outstanding", url: "/billing/sale/report/outstanding", action: ["READ"],  },
+      { name: "Billing sell Report", path: "/billing/account/billing-report", moduleName: "Billing Sell report", url: "/billing/sale/report/sell", action: ["READ"],  },
+      { name: "Receipt Report", path: "/billing/account/receipt-report", moduleName: "Receipt report", url: "/billing/sale/report/receipt", action: ["READ"],  },
     ]
      
   },
